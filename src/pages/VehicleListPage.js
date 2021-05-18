8918398d6fb748a088e893088b3d3f85736da5c9
Dawid Gacek <dawid.gacek451@outlook.com>
2021-05-18 14:57:48 +0200

    renamed VehicleList to VehicleListPage

--- src/pages/VehicleListPage.js
+++ src/pages/VehicleListPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import Button from '@material-ui/core/Button';
 
-export default function VehicleList() {
+export default function VehicleListPage() {
     function lol(){
         localStorage.removeItem("AUTH_TOKEN");
         axios.defaults.headers.common["Authorization"] = null;
