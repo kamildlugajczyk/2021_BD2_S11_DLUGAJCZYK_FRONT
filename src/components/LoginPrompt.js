97e099338f09056746130f4a1cc991ed98f2f45c
Dawid Gacek <dawid.gacek451@outlook.com>
2021-05-20 13:18:19 +0200

    made login prompt nice and centered

--- src/components/LoginPrompt.js
+++ src/components/LoginPrompt.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import {useState} from 'react';
+import { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import axios from 'axios';
+import { Container, CssBaseline, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    textInput: {
+        marginBottom: theme.spacing(1)
+    }
+}));
 
 export default function LoginPrompt() {
+    const classes = useStyles();
     const [login, setLogin] = useState();
     const [password, setPassword] = useState();
 
@@ -17,45 +31,54 @@ export default function LoginPrompt() {
                 password: password
             }
         })
-        .then((response) => {
-            localStorage.setItem(`AUTH_TOKEN`, response.data);
-            axios.defaults.headers.common["Authorization"] = response.data;
-            window.location.reload();
-        })
-        .catch(() => {
-            localStorage.setItem('AUTH_TOKEN', "Bearer nosiema");
-            window.location.reload();
-        });
+            .then((response) => {
+                localStorage.setItem(`AUTH_TOKEN`, response.data);
+                axios.defaults.headers.common["Authorization"] = response.data;
+                window.location.reload();
+            })
+            .catch(() => {
+                localStorage.setItem('AUTH_TOKEN', "Bearer nosiema");
+                window.location.reload();
+            });
     }
 
-    return(
-        <form>
-            <TextField
-                id="login"
-                label="Login"
-                name="login"
-                variant="outlined"
-                required
-                autoComplete="login"
-                onChange={(l) => { setLogin(l.target.value) }}
-            />
-            <TextField
-                id="password"
-                label="Password"
-                name="password"
-                type="password"
-                variant="outlined"
-                required
-                autoComplete="current-password"
-                onChange={(l) => { setPassword(l.target.value) }}
-            />
-            <Button 
-                onClick={doLogin}
-                variant="outlined"
-                size="large"
-            >
-                Login
-            </Button>
-        </form>
+    return (
+        <Container component="main" maxwidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <form>
+                    <TextField
+                        className={classes.textInput}
+                        id="login"
+                        label="Login"
+                        name="login"
+                        variant="outlined"
+                        required
+                        fullWidth
+                        autoComplete="login"
+                        onChange={(l) => { setLogin(l.target.value) }}
+                    />
+                    <TextField
+                        className={classes.textInput}
+                        id="password"
+                        label="Password"
+                        name="password"
+                        type="password"
+                        variant="outlined"
+                        required
+                        fullWidth
+                        autoComplete="current-password"
+                        onChange={(l) => { setPassword(l.target.value) }}
+                    />
+                    <Button
+                        onClick={doLogin}
+                        variant="contained"
+                        fullWidth
+                    >
+                        Login
+                    </Button>
+                </form>
+            </div>
+        </Container>
     );
 }
