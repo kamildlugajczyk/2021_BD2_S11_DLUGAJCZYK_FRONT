79aa2b8f2b87971ec7feffdeba69067cb80d5d90
Dawid Gacek <dawid.gacek451@outlook.com>
2021-06-01 20:39:12 +0200

    VehiclePicker now uses DataGrid instead of a List

--- src/components/VehiclePicker.js
+++ src/components/VehiclePicker.js
@@ -1,4 +1,5 @@
-import { List, ListItem, ListItemText } from '@material-ui/core';
+//import { List, ListItem, ListItemText } from '@material-ui/core';
+import { DataGrid } from '@material-ui/data-grid';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
@@ -6,12 +7,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setSelected, selectSelectedVehicleId } from '../redux/VehiclePickerSlice';
 import config from '../config';
 
+const columns = [
+    {field: 'id', headerName: 'ID', width: 100},
+    {field: 'model', headerName: 'Model', width: 200},
+    {field: 'type', headerName: 'Type', width: 120},
+    {field: 'purpose', headerName: 'Purpose', width: 300}
+]
+
 const useStyles = makeStyles((theme) => ({
     root: {
-      width: '400px',
-      maxWidth: 360,
-      backgroundColor: theme.palette.background.paper,
-      marginLeft: '200px'
+      width: (window.innerWidth-200)*0.4, //% values dont work for datagrid for some reason
+      marginLeft: '200px',
+      height: window.innerHeight
     },
   }));
 
@@ -26,7 +33,6 @@ export default function VehiclePicker(props) {
         axios({
             method: "GET",
             url: `${config.API_URL}${props.url}`,
-            //url: `https://fleet-management-develop.herokuapp.com${props.url}`,
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${localStorage.getItem("AUTH_TOKEN")}`
@@ -38,25 +44,41 @@ export default function VehiclePicker(props) {
         dispatch(setSelected(0));
     }, [props.url, dispatch]);
 
+    let rows = [];
+    vehicleList.forEach((vehicle) => {
+        rows = rows.concat([{
+            id: vehicle.id,
+            model: [vehicle.brandmodel.brand, vehicle.brandmodel.model].join(" "),
+            type: vehicle.type.name,
+            purpose: vehicle.purpose.name
+        }])
+    })
+
     return (
         <div className={classes.root}>
-            <List component="nav">
-                {vehicleList.map((vehicle) => {
-                    return (
-                    <ListItem
-                        button
-                        key={vehicle.id}
-                        selected={selectedVehicleId === vehicle.id}
-                        onClick={() => dispatch(setSelected(vehicle.id))}
-                    >
-                        <ListItemText 
-                            primary={[vehicle.brandmodel.brand, vehicle.brandmodel.model].join(" ")}
-                            secondary={[vehicle.type.name, vehicle.equipmentLevel, vehicle.purpose.name].join(" | ")} 
-                        />
-                    </ListItem>
-                    )
-                })}
-            </List>
+            <DataGrid rows={rows} columns={columns} disableMultipleSelection={true} onRowSelected={(row) => {dispatch(setSelected(row.data.id))}}/>
         </div>
     )
+
+    // return (
+    //     <div className={classes.root}>
+    //         <List component="nav">
+    //             {vehicleList.map((vehicle) => {
+    //                 return (
+    //                 <ListItem
+    //                     button
+    //                     key={vehicle.id}
+    //                     selected={selectedVehicleId === vehicle.id}
+    //                     onClick={() => dispatch(setSelected(vehicle.id))}
+    //                 >
+    //                     <ListItemText 
+    //                         primary={[vehicle.brandmodel.brand, vehicle.brandmodel.model].join(" ")}
+    //                         secondary={[vehicle.type.name, vehicle.equipmentLevel, vehicle.purpose.name].join(" | ")} 
+    //                     />
+    //                 </ListItem>
+    //                 )
+    //             })}
+    //         </List>
+    //     </div>
+    // )
 }
