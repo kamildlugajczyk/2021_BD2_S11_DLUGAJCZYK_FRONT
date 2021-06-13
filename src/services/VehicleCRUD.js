17d5a62a9f373f3d86a853930c63c9705449685c
Dawid Gacek <dawid.gacek451@outlook.com>
2021-06-13 12:34:30 +0200

    added wrapper functions for vehicle crud operations

---
+++ src/services/VehicleCRUD.js
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import config from '../config';
+
+
+
+// params:
+// {
+//   "avgFuelConsumption": Integer,
+//   "brandModelId": Integer,
+//   "equipmentLevel": String,
+//   "id": Integer,
+//   "mileage": Integer,
+//   "plates": String,
+//   "purposeId": Integer,
+//   "typeId": Integer,
+//   "vin": String
+// }
+export function addVehicle(params) {
+    return axios({
+        method: "POST",
+        url: `${config.API_URL}/vehicle`,
+        data: params,
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem("AUTH_TOKEN")}`
+        }
+    })
+}
+
+export function getVehicle(id) {
+    return axios({
+        method: "GET",
+        url: `${config.API_URL}/vehicle/${id}`,
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem("AUTH_TOKEN")}`
+        }
+    })
+}
+
+export function getAllVehicles() {
+    return axios({
+        method: "GET",
+        url: `${config.API_URL}/vehicle`,
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem("AUTH_TOKEN")}`
+        }
+    })
+}
+
+
+// params:
+// {
+//   "avgFuelConsumption": Integer,
+//   "brandModelId": Integer,
+//   "equipmentLevel": String,
+//   "id": Integer,
+//   "mileage": Integer,
+//   "plates": String,
+//   "purposeId": Integer,
+//   "typeId": Integer,
+//   "vin": String
+// }
+export function editVehicle(params) {
+    return axios({
+        method: "PUT",
+        url: `${config.API_URL}/vehicle/${id}`,
+        data: params,
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem("AUTH_TOKEN")}`
+        }
+    })
+}
+
+export function deleteVehicle(id) {
+    return axios({
+        method: "DELETE",
+        url: `${config.API_URL}/vehicle/${id}`,
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem("AUTH_TOKEN")}`
+        }
+    })
+}
