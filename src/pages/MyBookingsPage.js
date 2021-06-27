92e5f5cc4ab2e0ab7b3364c37ec9abdf396087de
Dawid Gacek <dawid.gacek451@outlook.com>
2021-06-27 19:10:57 +0200

    MyBookingsPage wip

--- src/pages/MyBookingsPage.js
+++ src/pages/MyBookingsPage.js
@@ -1,12 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import MenuBar from '../components/MenuBar';
-import VehiclePicker from '../components/VehiclePicker';
 import LoginPage from './LoginPage';
-import { CircularProgress, makeStyles } from '@material-ui/core';
+import { Button, CircularProgress, makeStyles } from '@material-ui/core';
 import { getMyPermissions } from '../services/UserAccount';
+import { useSelector } from 'react-redux';
+import { selectSelectedRentingId } from '../redux/RentingListSlice';
+import RentingList from '../components/RentingList';
+import { Modal } from '@material-ui/core';
+import AddOperationalCostDialog from '../components/dialogs/AddOperationalCostDialog';
 
 
 const useStyles = makeStyles((theme) => ({
+    root: {
+        display: "flex",
+        position: "fixed",
+        height: "100%",
+        width: "100%"
+    },
+    content: {
+        display: 'flex',
+        flexDirection: "column",
+        flex: "1"
+    },
+    list: {
+        height: "100%",
+        paddingRight: 15,
+        flex: "1"
+    },
+    buttons: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-around",
+        paddingTop: 20,
+        paddingBottom: 30,
+        width: "50%",
+        marginLeft: "50%",
+        transform: "translate(-50%)"
+    },
     loading: {
         position: "fixed",
         top: "50%",
@@ -15,11 +45,21 @@ const useStyles = makeStyles((theme) => ({
         display: "flex",
         alignItems: "center",
         justifyContent: "center"
-    }
+    },
+    modal: {
+        position: 'fixed',
+        width: "25%",
+        backgroundColor: theme.palette.background.paper,
+        border: '1px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(3, 4, 3),
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)"
+    },
 }))
 
 export default function MyBookingsGate() {
-
     const classes = useStyles();
     const [isTokenValid, setIsTokenValid] = useState(null);
 
@@ -51,11 +91,63 @@ export default function MyBookingsGate() {
 }
 
 function MyBookingsPage() {
+    const classes = useStyles();
+    const selectedRentingId = useSelector(selectSelectedRentingId);
+
+    const [viewUpdater, setViewUpdater] = useState(false);
+
+    const [isCostModalOpen, setIsCostModalOpen] = useState(false);
+    const [isFinishModalOpen, setIsFinishModalOpen] = useState(false);
 
     return (
-        <div>
+        <div className={classes.root}>
             <MenuBar selected="my-bookings" />
-            <VehiclePicker url="/get-my-bookings" />
+            <div className={classes.content}>
+                <div className={classes.list}>
+                    <RentingList updater={viewUpdater} />
+                </div>
+                {selectedRentingId !== 0 &&
+                    <div className={classes.buttons}>
+                        <Button
+                            variant="contained"
+                            onClick={() => { setIsCostModalOpen(true) }}
+                        >
+                            Add operational cost
+                        </Button>
+                        <Modal
+                            open={isCostModalOpen}
+                            onClose={() => { setIsCostModalOpen(false) }}
+                        >
+                            <div className={classes.modal}>
+                                <AddOperationalCostDialog
+                                    onClose={
+                                        (isListChanged) => {
+                                            setIsCostModalOpen(false);
+                                            if (isListChanged) {
+                                                setViewUpdater(!viewUpdater);
+                                            }
+                                        }
+                                    }
+                                />
+                            </div>
+                        </Modal>
+                        <Button
+                            variant="contained"
+                            onClick={() => { setIsFinishModalOpen(true) }}
+                        >
+                            Finish renting
+                        </Button>
+                        <Modal
+                            open={isFinishModalOpen}
+                            onClose={() => { setIsFinishModalOpen(false) }}
+                        >
+                            <div className={classes.modal}>
+                                tets
+                            </div>
+                        </Modal>
+                    </div>
+                }
+            </div>
         </div>
     )
 }
